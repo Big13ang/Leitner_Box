91e86b5883d4eef6f3746381ee12ccdf70bcda57
John Doe <mmd.ayin.2020@gmail.com>
2023-08-17 22:18:14 +0330

    Refactor Router and fix Set event in SPA

--- main.js
+++ main.js
@@ -66,67 +66,7 @@ class DictationCard extends Card {
 
 // }
 
-class Router {
-    sideBar;
-    constructor() {
-        this.sideBar = $.querySelector('#sidebar');
-        this.sideBar.addEventListener('click', this.onClickHandler.bind(this));
-    }
 
-    onClickHandler(e) {
-        const { target } = e;
-        if (!target.matches(".nav-link")) return;
-        e.preventDefault();
-        this.urlRoute();
-    }
-
-    urlRoutes = {
-        404: {
-            template: "/pages/404.html",
-            title: "404 | Page not found !"
-        },
-        "/": {
-            template: "/pages/overview.html",
-            title: "Overview"
-        },
-        "/add-card": {
-            template: "/pages/add-card.html",
-            title: "Add new Card"
-        },
-        "/progressbar": {
-            template: "/pages/progressbar.html",
-            title: "Your progress in learning !"
-        },
-        "/info": {
-            template: "/pages/info.html",
-            title: "Application information"
-        }
-    }
-
-    urlRoute(event) {
-        event = event || window.event;
-        event.preventDefault();
-        window.history.pushState({}, "", event.target.href);
-        this.urlLocationHandler();
-    }
-
-    async urlLocationHandler() {
-        const location = window.location.pathname;
-        if (location.length == 0) {
-            location = "/";
-        }
-        const route = this.urlRoutes[location] || this.urlRoutes[404];
-        const html = await fetch(route.template).then(response => response.text());
-        $.getElementById("main").innerHTML = html;
-
-        if (location == "/add-card") {
-            app.initAddCard();
-        }
-        window.onpopstate = this.urlLocationHandler;
-        window.route = this.urlRoute;
-        this.urlLocationHandler();
-    }
-}
 
 class App {
     cardsList = [[], [], [], [], []];
@@ -219,5 +159,4 @@ class App {
     }
 }
 
-let router = new Router();
 let app = new App();
